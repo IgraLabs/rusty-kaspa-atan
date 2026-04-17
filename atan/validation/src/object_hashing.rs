b128fca705c043e6c4fcf890171b2cf7845b59d2
Mike Zak <feanorr@gmail.com>
2026-04-17 14:38:22 +0300

    [ENG-1002] Combine mergeset_context_hash and miner_payload_root into state_root

--- atan/validation/src/object_hashing.rs
+++ atan/validation/src/object_hashing.rs
@@ -3,23 +3,11 @@ use kaspa_hashes::Hash;
 use kaspa_seq_commit::hashing::{mergeset_context_hash, miner_payload_leaf, miner_payload_root};
 use kaspa_seq_commit::types::{MergesetContext as SeqCommitMergesetContext, MinerPayloadLeafInput};
 
-pub(crate) trait Rooter {
-    fn root(&self) -> Hash;
-}
 pub(crate) trait Hasher {
     fn hash(&self) -> Hash;
 }
-impl Rooter for &[MinerPayload] {
-    fn root(&self) -> Hash {
-        let payload_leaves = self.iter().map(|miner_payload|
-            miner_payload_leaf(&MinerPayloadLeafInput {
-                block_hash: &miner_payload.block_hash,
-                blue_work_bytes: &miner_payload.blue_work.to_le_bytes(),
-                payload: &miner_payload.payload,
-            }));
-
-        miner_payload_root(payload_leaves)
-    }
+pub(crate) trait Rooter {
+    fn root(&self) -> Hash;
 }
 
 impl Hasher for MergesetContext {
@@ -29,5 +17,18 @@ impl Hasher for MergesetContext {
             daa_score: self.daa_score,
             blue_score: self.blue_score,
         })
+    }
+}
+
+impl Rooter for Vec<MinerPayload> {
+    fn root(&self) -> Hash {
+        let payload_leaves = self.iter().map(|miner_payload|
+            miner_payload_leaf(&MinerPayloadLeafInput {
+                block_hash: &miner_payload.block_hash,
+                blue_work_bytes: &miner_payload.blue_work.to_le_bytes(),
+                payload: &miner_payload.payload,
+            }));
+
+        miner_payload_root(payload_leaves)
     }
 }
--- atan/validation/src/validate_chain_block.rs
+++ atan/validation/src/validate_chain_block.rs
@@ -1,7 +1,13 @@
+use crate::object_hashing::{Hasher, Rooter};
 use kaspa_atan_core::model::ChainBlock;
 use kaspa_hashes::Hash;
+use kaspa_seq_commit::hashing::payload_and_context_digest;
 
-pub fn validate_chain_block(chain_block: ChainBlock, selected_parent_sequencing_commitment: Hash) {}
+pub fn validate_chain_block(chain_block: ChainBlock, selected_parent_sequencing_commitment: Hash) {
+    let mergeset_context_hash = chain_block.base().merge_set_context.hash();
+    let miner_payload_root = chain_block.base().miner_payloads.root();
+    let state_root = payload_and_context_digest(&mergeset_context_hash, &miner_payload_root);
+}
 
 #[cfg(test)]
 mod tests {}
