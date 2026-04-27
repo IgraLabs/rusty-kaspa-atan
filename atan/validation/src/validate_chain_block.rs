5d2db398063c5aab80a909eb982da124d30e39ac
Mike Zak <feanorr@gmail.com>
2026-04-27 14:19:56 +0300

    [ENG-1002] Add comments

--- atan/validation/src/validate_chain_block.rs
+++ atan/validation/src/validate_chain_block.rs
@@ -5,11 +5,16 @@ use kaspa_atan_core::model::ChainBlock;
 use kaspa_hashes::Hash;
 use kaspa_seq_commit::types::LaneId;
 
+/// Provides validation services for ChainBlocks
 pub struct AtanValidator {
     pub(crate) lane_id: Option<LaneId>,
 }
 
 impl AtanValidator {
+    /// Creates a new AtanValidator
+    ///
+    /// # Arguments
+    /// * `lane_id` - The lane_id this ATAN keeps. Pass None if this ATAN keeps all laneIDs.
     pub fn new(lane_id: Option<LaneId>) -> Self {
         Self {
             lane_id,
@@ -18,6 +23,17 @@ impl AtanValidator {
 }
 
 impl AtanValidator {
+    /// Validates a ChainBlock.
+    ///
+    /// # Arguments
+    /// * `chain_block` - the ChainBlock to validate.
+    /// * `selected_parent_sequencing_commitment` - the SequencingCommitment field for this ChainBlock's selected parent
+    ///
+    /// # Returns
+    /// * `Ok(())` if the block is valid
+    ///
+    /// # Errors
+    /// `AtanError::Validation(_)` - If any of the validation steps fail.
     pub fn validate_chain_block(&self, chain_block: &ChainBlock, selected_parent_sequencing_commitment: &Hash) -> AtanResult<()> {
         let expected_sequencing_commitment = self.calculate_sequencing_commitment(chain_block, selected_parent_sequencing_commitment)?;
 
