3f6018d6821023ff717a7f55a773c1b2eec75928
Svarog <feanorr@gmail.com>
2026-05-21 09:57:01 +0300

    [ENG-1002] Add validation logic (#3)
    * [ENG-1003] Apply github workflows only to atan (#2)
    
    * [ENG-1002] Add `active_lanes_root` field to BareChainBlock
    
    * [ENG-1002] Add parent_ref field to LaneActivityProof
    
    * [ENG-1002] Add pub to fields of multiple model structs that missed it
    
    * [ENG-1002] Add validate_chain_block skeleton
    
    * [ENG-1002] Implement MinerPayloadRoot and MergesetContextHash calculation.
    
    * [ENG-1002] Combine mergeset_context_hash and miner_payload_root into state_root
    
    * [ENG-1002] Define basic AtanResult and AtanError
    
    * [ENG-1002] Finish writing validate_chain_block
    
    * [ENG-1002] Move calculate_sequencing_commitment to object_hashing.rs
    
    * [ENG-1002] Finish validation logic for specific-lane atan
    
    * Extend validation for all-lane ATANs
    
    * [ENG-1002] Pass everything by-reference in validation. To make things more performant, and in accordance to rest of KIP-21 conventions
    
    * [ENG-1002] Add comments
    
    * [ENG-1002] Begin document validation steps
    
    * [ENG-1002] Add selected_parent_sequencing_commitment to ChainBlock
    
    * [ENG-1002] Add {validate_historical, validate_recent}_chain_block_list
    
    * [ENG-1002] Finish writing validation steps documentation
    
    * [ENG-1002] Multiple comment improvements
    
    * [ENG-1002] Make callbacks return the ChainBlock, then validator will know which fields to use
    
    * [ENG-1002] Update to latest KIP-21 spec

---
+++ atan/core/src/errors.rs
@@ -0,0 +1,72 @@
+use kaspa_hashes::Hash;
+use kaspa_seq_commit::types::LaneId;
+use kaspa_smt::proof::SmtProofError;
+use std::fmt::{Debug, Display};
+use thiserror::Error;
+
+pub type AtanResult<T> = Result<T, AtanError>;
+
+#[derive(Error, Debug, Clone)]
+pub enum AtanError {
+    #[error("Validation Error: {0}")]
+    Validation(ValidationError),
+    #[error("System Error: {0}")]
+    System(SystemError),
+}
+
+#[derive(Error, Debug, Clone)]
+pub enum ValidationError {
+    #[error("Invalid sequencing commitment: Expected: {0}, Actual: {1}")]
+    InvalidSequencingCommitment(Expected<Hash>, Actual<Hash>),
+    #[error("The provided LaneSMTProof isn't valid: {0}")]
+    InvalidLaneSMTProof(SmtProofError),
+    #[error("Got 0 lanes in a ChainBlock")]
+    EmptyLanesWithActivityDigests,
+    #[error("Got {0} lanes in a ChainBlock, when 1 is expected")]
+    InvalidNumberOfLanesWithActivityDigests(Actual<usize>),
+    #[error("Invalid laneID: Expected: {0}, Actual: {1}")]
+    InvalidLaneID(Expected<LaneId>, Actual<LaneId>),
+    #[error("ActiveLanesRoot doesn't match: For lane {0:?} it's {1}; For lane {2:?} it's {3}")]
+    UnmatchingActiveLanesRootForDifferentLanes(LaneId, Hash, LaneId, Hash),
+    #[error(
+        "Recent block invalid because it doesn't connect to existing chain: Expected selected parent sequencing commitment: {0}. Actual: {1}"
+    )]
+    RecentBlockDoesntConnect(Expected<Hash>, Actual<Hash>),
+    #[error("Historical block invalid because it doesn't connect to existing chain: Expected sequencing commitment: {0}. Actual: {1}")]
+    HistoricalBlockDoesntConnect(Expected<Hash>, Actual<Hash>),
+}
+
+#[derive(Error, Debug, Clone)]
+pub enum SystemError {}
+
+/// Wraps the expected value in an error.
+#[derive(Debug, Clone, PartialEq)]
+pub struct Expected<T>(pub T);
+
+/// Wraps the actual value in an error.
+#[derive(Debug, Clone, PartialEq)]
+pub struct Actual<T>(pub T);
+
+impl<T: Debug> Display for Actual<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Actual: {:?}", self.0)
+    }
+}
+
+impl<T: Debug> Display for Expected<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Expected: {:?}", self.0)
+    }
+}
+
+impl PartialEq<Hash> for Expected<Hash> {
+    fn eq(&self, other: &Hash) -> bool {
+        self.0 == *other
+    }
+}
+
+impl PartialEq<Hash> for Actual<Hash> {
+    fn eq(&self, other: &Hash) -> bool {
+        self.0 == *other
+    }
+}
--- atan/core/src/model.rs
+++ atan/core/src/model.rs
@@ -3,10 +3,11 @@
 use kaspa_consensus_core::BlueWorkType;
 use kaspa_consensus_core::tx::Transaction;
 use kaspa_hashes::Hash;
+use kaspa_seq_commit::types::LaneId;
 use kaspa_smt::proof::OwnedSmtProof;
 
 /// Represents a block in the selected parent chain.
-/// Including the MergeSetContext and all information required to prove the ChainBlock's validity,
+/// Including the MergesetContext and all information required to prove the ChainBlock's validity,
 /// assuming proper connection to a prior or consequent block.
 ///
 /// There are 3 types of chain blocks, a single ATAN will always keep only one of these types,
@@ -15,9 +16,9 @@ use kaspa_smt::proof::OwnedSmtProof;
 /// 2. WithTransactionIDs - also contains the transaction ids and versions.
 /// 3. WithTransactions - also contains the transactions themselves.
 #[allow(dead_code)] // TODO: Remove this once this code is used
-enum ChainBlock {
+pub enum ChainBlock {
     Bare(BareChainBlock),
-    WithTransactionIDs(ChainBlockWithTransactionIDs),
+    WithActivityDigest(ChainBlockWithActivityDigests),
     WithTransactions(ChainBlockWithTransactions),
 }
 
@@ -28,8 +29,10 @@ pub struct ChainBlockBase {
     pub block_hash: Hash,
     /// The sequencing commitment of the chain block as defined by KIP-21.
     pub sequencing_commitment: Hash,
-    /// The MergeSetContext of the chain block as defined by KIP-21
-    pub merge_set_context: MergeSetContext,
+    /// The sequencing commitment of the chain block's selected parent.
+    pub selected_parent_sequencing_commitment: Hash,
+    /// The MergesetContext of the chain block as defined by KIP-21
+    pub merge_set_context: MergesetContext,
     /// The fields consisting MinerPayloadRoot as defined by KIP-21.
     /// One `MinerPayload` per merged block, in merge order.
     pub miner_payloads: Vec<MinerPayload>,
@@ -43,23 +46,48 @@ pub struct ChainBlockBase {
 pub struct BareChainBlock {
     /// The common fields all types of chain blocks contain.
     pub base: ChainBlockBase,
+    /// The ActiveLanesRoot field as defined by KIP-21.
+    pub active_lanes_root: Hash,
+}
+
+/// Contains the transaction activity (ActivityDigest only, no full transactions) in a single lane
+/// in a single chain block as well as the data required to prove its validity.
+#[derive(Clone, Debug, PartialEq)]
+pub struct LaneActivityDigestsWithProof {
+    /// The ID of this lane
+    pub lane_id: LaneId,
+    /// A list of ActivityDigests merged in this ChainBlock in this lane.
+    pub activity_digests: Vec<ActivityDigest>,
+    /// All information required to prove that the `activity_digests` list corresponds to the
+    /// stated `sequencing_commitment`.
+    pub lane_proof: LaneActivityProof,
 }
 
 /// Represents a chain block in an ATAN that only keeps transaction IDs.
 /// Contains everything a `BareChainBlock` contains, as well as a list of ActivityDigests
 /// and the data needed to prove its validity.
 #[derive(Clone, Debug, PartialEq)]
-pub struct ChainBlockWithTransactionIDs {
+pub struct ChainBlockWithActivityDigests {
     /// The common fields all types of chain blocks contain.
     pub base: ChainBlockBase,
-    /// List of the transaction IDs accepted by this chain block.
-    /// If this ATAN holds a single lane, only transaction IDs of active lane will be included.
-    /// If this ATAN holds all lanes, all accepted transactions will be included.
-    pub activity_digests: Vec<ActivityDigest>,
-    /// All information required to prove that the `transaction_ids` list corresponds to the
+    /// List of the activity digests merged by this chain block, grouped by LaneId, including proofs for their validity.
+    /// To keep order intact this is a Vec of structs rather than a HashMap.
+    /// If this ATAN holds a single lane, a single entry with the activity digests of the selected lane will be included.
+    /// If this ATAN holds all lanes, all accepted transactions will be included, with one entry per lane with activity.
+    pub activity_digests_with_proofs: Vec<LaneActivityDigestsWithProof>,
+}
+
+/// Contains the transaction activity (including the full transactions) in a single lane
+/// in a single chain block as well as the data required to prove its validity.
+#[derive(Clone, Debug, PartialEq)]
+pub struct LaneTransactionsWithProof {
+    /// The ID of this lane
+    pub lane_id: LaneId,
+    /// A list of Transactions merged in this ChainBlock in this lane.
+    pub transactions: Vec<TransactionWithMergeIndex>,
+    /// All information required to prove that the `transactions` list corresponds to the
     /// stated `sequencing_commitment`.
-    /// Will be `None` if this ATAN keeps all lanes.
-    pub lane_proof: Option<LaneActivityProof>,
+    pub lane_proof: LaneActivityProof,
 }
 
 /// Represents a chain block in an ATAN that keeps full transaction data.
@@ -69,48 +97,59 @@ pub struct ChainBlockWithTransactionIDs {
 pub struct ChainBlockWithTransactions {
     /// The common fields all types of chain blocks contain.
     pub base: ChainBlockBase,
-    /// List of the transaction accepted by this chain block.
-    /// If this ATAN holds a single lane, only transaction of active lane will be included.
-    /// If this ATAN holds all lanes, all accepted transactions will be included.
-    pub transactions: Vec<TransactionWithMergeIndex>,
-    /// All information required to prove that the `transactions` list corresponds to the
-    /// stated `sequencing_commitment`
-    /// Will be None if this ATAN keeps all lanes.
-    pub lane_proof: Option<LaneActivityProof>,
+    /// List of the transaction merged by this chain block, grouped by LaneId, including proofs for their validity.
+    /// To keep order intact this is a Vec of structs rather than a HashMap.
+    /// If this ATAN holds a single lane, a single entry with the transactions of the selected lane will be included.
+    /// If this ATAN holds all lanes, all accepted transactions will be included, with one entry per lane with activity.
+    pub transactions_with_proofs: Vec<LaneTransactionsWithProof>,
 }
 impl ChainBlock {
     /// Returns the `ChainBlockBase` part of this ChainBlock.
-    #[allow(dead_code)] // TODO: Remove this once this code is used
     pub fn base(&self) -> &ChainBlockBase {
         match self {
             ChainBlock::Bare(chain_block) => &chain_block.base,
-            ChainBlock::WithTransactionIDs(chain_block) => &chain_block.base,
+            ChainBlock::WithActivityDigest(chain_block) => &chain_block.base,
             ChainBlock::WithTransactions(chain_block) => &chain_block.base,
         }
     }
 }
 
-/// The MergeSetContext as defined in KIP-21.
+impl LaneTransactionsWithProof {
+    pub fn to_activity_digests(&self) -> LaneActivityDigestsWithProof {
+        LaneActivityDigestsWithProof {
+            lane_id: self.lane_id,
+            activity_digests: self.transactions.iter().map(|transaction| transaction.activity_digest()).collect(),
+            lane_proof: self.lane_proof.clone(),
+        }
+    }
+}
+impl ChainBlockWithTransactions {
+    pub fn activity_digests_with_proofs(&self) -> Vec<LaneActivityDigestsWithProof> {
+        self.transactions_with_proofs.iter().map(|lane_transactions| lane_transactions.to_activity_digests()).collect()
+    }
+}
+
+/// The MergesetContext as defined in KIP-21.
 /// Contains the consensus parameters of the ChainBlock that the Sequencing Commitment commits to.
 #[derive(Clone, Debug, PartialEq)]
-pub struct MergeSetContext {
+pub struct MergesetContext {
     /// The chain block's timestamp.
-    timestamp: u64,
+    pub timestamp: u64,
     /// The chain block's daa score.
-    daa_score: u64,
+    pub daa_score: u64,
     /// The chain block's blue score.
-    blue_score: u64,
+    pub blue_score: u64,
 }
 
 /// The fields consisting MinerPayloadRoot as defined by KIP-21.
 #[derive(Clone, Debug, PartialEq)]
 pub struct MinerPayload {
     /// The merged block's hash.
-    block_hash: Hash,
+    pub block_hash: Hash,
     /// The merged block's blue work.
-    blue_work: BlueWorkType,
+    pub blue_work: BlueWorkType,
     /// The merged block's coinbase transaction payload.
-    payload: Vec<u8>,
+    pub payload: Vec<u8>,
 }
 
 /// Represents the fields going into activity_digest as defined in KIP-21.
@@ -122,7 +161,7 @@ pub struct ActivityDigest {
     /// The version of the transaction.
     pub version: u16,
     /// The merge_index of the transaction within the ChainBlock's merge set.
-    pub merge_index: u64,
+    pub merge_index: u32,
 }
 
 /// Contains all the data required to prove the validity of a list of `ActivityDigest`s
@@ -130,9 +169,11 @@ pub struct ActivityDigest {
 #[derive(Clone, Debug, PartialEq)]
 pub struct LaneActivityProof {
     /// The blue score of the highest chain block that has merged transactions in this lane.
-    last_touch_blue_score: u64,
+    pub last_touched_blue_score: u64,
+    /// The ParentRef field as defined in KIP-21.
+    pub parent_ref: Hash,
     /// The SMT proof for this lane's payload within ActiveLanesRoot.
-    proof: OwnedSmtProof,
+    pub smt_proof: OwnedSmtProof,
 }
 
 /// Represents a transaction with its merge index.
@@ -141,7 +182,7 @@ pub struct TransactionWithMergeIndex {
     /// The transaction.
     pub transaction: Transaction,
     /// The transaction's merge index.
-    pub merge_index: u64,
+    pub merge_index: u32,
 }
 
 impl TransactionWithMergeIndex {
